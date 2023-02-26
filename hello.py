c4b28f0cdc2ea7399c838ba3e4e1e7fc725b3448
David Campos <dcamposchina@gmail.com>
2023-02-26 17:05:44 +0000

    after video 5

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 #create an instance of flask
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "dpc123"
+
+#create a form class
+class NamerForm(FlaskForm):
+	name = StringField("What's your name?", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+
 
 #create a route decorator
 @app.route('/')
@@ -25,10 +35,7 @@ def index():
 	stuff = "This is bold text"
 	
 	fav_pizza = ['Pepperoni','Cheese', 'Mushroom', 41]
-	return render_template("index.html",
-		first_name=first_name,
-		stuff =stuff,
-		fav_pizza=fav_pizza)
+	return render_template("index.html", first_name=first_name, stuff=stuff, fav_pizza=fav_pizza)
 
 #localhost:5000/user/john
 @app.route('/user/<name>')
@@ -47,5 +54,19 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
 	return render_template("500.html"), 500
+
+
+
+#create name page
+@app.route('/name',methods=['GET', 'POST'])
+def name():
+	name = None
+	form = NamerForm()
+	#Validate form
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+
+	return render_template("name.html", name=name,form=form)
 
 
